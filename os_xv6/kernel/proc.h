f5dd92d15a8067902234250cb2605edf636d4a3f
michael zaslavski <52536729+nfnmod@users.noreply.github.com>
2022-04-13 14:12:06 +0300

    updated proc struct for FCFS

--- os_xv6/kernel/proc.h
+++ os_xv6/kernel/proc.h
@@ -92,6 +92,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  uint last_runnable_time      // value of global ticks when the procces was running the last time 
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
